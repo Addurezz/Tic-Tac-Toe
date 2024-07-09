980af29e5c89e2a2bf421ab3df9bf57bfb6c5c0e
Wilhelm Jahn <wilhelmjahn2005@gmail.com>
2024-07-10 00:48:41 +0200

    add restart fucntion, as well as some styling and clean up of code

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const game = (() => {
     ];
 
     let currentPlayer = "X";
+    let gameActive = true;
 
     const checkWin = () => {
         const winPatterns = [
@@ -25,6 +26,7 @@ const game = (() => {
         for (const pattern of winPatterns) {
             const [a, b, c] = pattern;
             if (board[a.r][a.c] && board[a.r][a.c] === board[b.r][b.c] && board[a.r][a.c] === board[c.r][c.c]) {
+                gameActive = false;
                 return true;
             }
         }
@@ -32,17 +34,86 @@ const game = (() => {
     }
 
     const boardIsFull = () => {
-        for (const row of game.board) {
+        for (const row of board) {
             for (const cell of row) {
                 if (cell ==='') {
                     return false
                 }
             }
         }
+        gameActive = false;
         return true;
     }
     
-    return {board,currentPlayer,boardIsFull,checkWin}
+    const display = (cell) => {
+        if (!gameActive) return;
+
+        const h2 = document.querySelector("h2");
+        const row = parseInt(cell.getAttribute("data-row"));
+        const col = parseInt(cell.getAttribute("data-col"));
+        
+        if (cell.textContent === "") {
+            const currentPlayerObject = game.currentPlayer === "X" ? player1 : player2;
+            const playerTurnText = currentPlayerObject.playerName === player1.playerName ? player2.playerName : player1.playerName;
+            h2.textContent = `${playerTurnText}'s turn. Choose a cell!`
+            cell.textContent = game.currentPlayer;
+            cell.style.color = currentPlayerObject === player1 ? "blue" : "red";
+            currentPlayerObject.takeTurn(row, col);
+            if (game.checkWin()) {
+                h2.textContent = `${currentPlayerObject.playerName} won.`;
+                disableCells();
+                createRestartBtn();
+            }
+
+            else if (boardIsFull()) {
+                h2.textContent = "It's a Draw";
+                disableCells();
+                createRestartBtn();
+            }
+        }
+    };
+
+    const disableCells = () => {
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => cell.removeEventListener("click", cell.displayListener()));
+    };
+    
+    const createRestartBtn = () => {
+        const body = document.querySelector("body");
+        const resetBtn = document.createElement("button");
+        resetBtn.textContent = "Restart";
+        resetBtn.addEventListener("click",() => init());
+        body.appendChild(resetBtn);
+    };
+    
+    const init = () => {
+        const h2 = document.querySelector("h2");
+        const resetBtn = document.querySelector("button");
+        h2.textContent = `${player1.playerName}, start by selecting a cell below!`;
+        
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach((cell) => {
+            cell.displayListener = () => display(cell);
+            cell.addEventListener("click", cell.displayListener);
+            cell.textContent = "";
+        });
+        
+        for(let row = 0; row < board.length; row++) {
+            for(let col = 0; col < board[row].length; col++ ) {
+                board[row][col] = "";
+            }
+        }
+        
+        gameActive = true;
+        currentPlayer = "X";
+        
+        if(resetBtn) {
+            resetBtn.remove();
+        }
+    };
+    
+
+    return {board,currentPlayer,boardIsFull,checkWin,display,init}
 })()
 
 const player = (playerName) => {
@@ -51,14 +122,10 @@ const player = (playerName) => {
             if (game.board[row-1][column-1] == "") {
                 game.board[row-1][column-1] = game.currentPlayer;
                 game.currentPlayer = game.currentPlayer === "X" ? "O" : "X";
-                if (game.checkWin()) {
-                    return `${playerName} won.`
-                }
             }
             else {
                 alert("This cell already has been selected, choose another one")
             }
-            console.log(game.board);
         }
         else if (game.boardIsFull()) {
             alert("No more space. It's a draw.")
@@ -67,6 +134,21 @@ const player = (playerName) => {
     return {playerName, takeTurn}
 }
 
-const player1 = player("Timo");
-const player2 = player("Wilhelm");
+let player1;
+let player2;
+
+const inputPlayer1 = document.querySelector("#player1");
+const inputPlayer2 = document.querySelector("#player2");
+
+inputPlayer1.addEventListener("input", (event) => {
+    const player1InputName = event.target.value;
+    player1 = player(player1InputName);
+    game.init();
+})
+
+inputPlayer2.addEventListener("input", (event) => {
+    const player2InputName = event.target.value;
+    player2 = player(player2InputName);
+    game.init();
+})
 
